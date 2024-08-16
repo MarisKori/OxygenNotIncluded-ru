5fb6963bed936db2614eb723870e643b11b1b425
Maris <maris.kori@gmail.com>
2024-08-16 12:14:09 +0200

    626616

--- docs/preview.js
+++ docs/preview.js
@@ -1,46 +1,231 @@
 db = [
 {
-	msgctxt: "STRINGS.LORE.BUILDINGS.SET_OBJECT.BODY.CONTAINER1",
-	msgid: `<smallcaps><b>Reminder to All Personnel!</b>\\nFrom: <b>Admin</b><alpha=#AA><admin@gravitas.nova></color>\\nTO: <b>All</b></smallcaps>\\n------------------\\n<indent=5%>\\nTo all employees;\\n\\nThe Gravitas Facility does not officially host a \\"Bring Your Child To Work Day\\" office event. We ask that personnel please do not bring children into top secret research departments, and especially that they do not let said children play with the high velocity particle accelerators.\\n\\nWe thank you for your understanding.</indent>`,
+	msgctxt: "STRINGS.BUILDINGS.PREFABS.GEOTHERMALVENT.BLOCKED_DESC",
+	msgid: `Blocked geo vents can be cleared by pumping in <link=\\"ELEMENTSLIQUID\\">liquids</link> that are hot enough to melt <link=\\"LEAD\\">Lead</link>.`,
+	msgstr: `Закупоренная экспульсивная камера может быть <color=#C38802>изнутри</color> прочищена <link=\\"ELEMENTSLIQUID\\">жидкостями</link>, которые достаточно горячие, чтобы расплавить <link=\\"LEAD\\">свинец</link>.`,
+	klei: `Засоренные геоклапаны можно прочистить с помощью <link=\\"ELEMENTSLIQUID\\">жидкостей</link>, нагретых до температуры плавления <link=\\"LEAD\\">свинца</link>.`,
+	mem: [
+		{
+			msgid: `Blocked geo vents can be cleared using <link=\\"ELEMENTSLIQUID\\">liquids</link> that are hot enough to melt <link=\\"LEAD\\">Lead</link>.`,
+			msgstr: `Закупоренная экспульсивная камера может быть <color=#C38802>изнутри</color> прочищена <link=\\"ELEMENTSLIQUID\\">жидкостями</link>, которые достаточно горячие, чтобы расплавить <link=\\"LEAD\\">свинец</link>.`,
+		},
+	],
+},
+{
+	msgctxt: "STRINGS.COLONY_ACHIEVEMENTS.MISC_REQUIREMENTS.WINCONDITION_ARTIFACTS_DESCRIPTION",
+	msgid: `Uncover the past to secure your future by fulfilling the requirements of the Exploration Imperative.`,
+	msgstr: `Открывайте прошлое для будущей безопасности, чтобы выполнить условия императива исследований.`,
+	klei: `Раскройте тайны прошлого, чтобы защитить свое будущее, выполнив условия первостепенной задачи исследования.`,
+	mem: [
+		{
+			msgid: `Uncover the past to secure your future by fullfilling the requirements of the Exploration Imperative.`,
+			msgstr: `Открывайте прошлое для будущей безопасности, чтобы выполнить условия императива исследований.`,
+		},
+	],
+},
+{
+	msgctxt: "STRINGS.CREATURES.MODIFIERS.CRITTER_TEMPERATURE_DEADLY.NAME",
+	msgid: `Deadly Temperature`,
 	msgstr: ``,
 },
 {
-	msgctxt: "STRINGS.LORE.BUILDINGS.SET_OBJECT.BODY.SIGNATURE1",
-	msgid: `\\n-Admin\\n<size=11>The Gravitas Facility</size>\\n------------------\\n`,
+	msgctxt: "STRINGS.CREATURES.MODIFIERS.CRITTER_TEMPERATURE_UNCOMFORTABLE.NAME",
+	msgid: `Uncomfortable Body Temperature`,
 	msgstr: ``,
 },
 {
-	msgctxt: "STRINGS.LORE.BUILDINGS.SET_OBJECT.SUBTITLE",
-	msgid: `SUBJECT: IMPORTANT`,
+	msgctxt: "STRINGS.DUPLICANTS.STATUSITEMS.COLD.TOOLTIP",
+	msgid: `This Duplicant cannot retain enough heat to stay warm and may be under-insulated for this area\\n\\nThey will begin to recover shortly after they leave this area\\n\\nStress: <b>{StressModification}</b>\\nStamina: <b>{StaminaModification}</b>\\nAthletics: <b>{AthleticsModification}</b>\\n\\nCurrent Environmental Exchange: <b>{currentTransferWattage}</b>\\n\\nInsulation Thickness: {conductivityBarrier}`,
+	msgstr: `
+Этот дубликант не может сохранить достаточно тепла, чтобы контролировать его и может быть изолирован от этой области\\n
+Стресс: <b>{StressModification}</b>\\n
+\\n
+Выносливость: <b>{StaminaModification}</b>\\n
+\\n
+Атлетичность: <b>{AthleticsModification}</b>\\n
+\\n
+Текущий температурный обмен: <b>{currentTransferWattage}</b>\\n
+\\n
+Толщина изоляции: {conductivityBarrier}`,
+	klei: `
+Этот дубликант теряет тепло и мерзнет. Возможно, ему не хватает теплоизоляции для этой зоны\\n
+\\n
+Стресс: <b>{StressModification}</b>\\n
+Выносливость: <b>{StaminaModification}</b>\\n
+Физическая форма: <b>{AthleticsModification}</b>\\n
+\\n
+Нынешняя скорость теплообмена с окружающей средой: <b>{currentTransferWattage}</b>\\n
+\\n
+Толщина теплоизоляции: {conductivityBarrier}`,
+	mem: [
+		{
+			msgid: `This Duplicant cannot retain enough heat to stay warm and may be under-insulated for this area\\n\\nStress: <b>{StressModification}</b>\\nStamina: <b>{StaminaModification}</b>\\nAthletics: <b>{AthleticsModification}</b>\\n\\nCurrent Environmental Exchange: <b>{currentTransferWattage}</b>\\n\\nInsulation Thickness: {conductivityBarrier}`,
+			msgstr: `
+Этот дубликант не может сохранить достаточно тепла, чтобы контролировать его и может быть изолирован от этой области\\n
+Стресс: <b>{StressModification}</b>\\n
+\\n
+Выносливость: <b>{StaminaModification}</b>\\n
+\\n
+Атлетичность: <b>{AthleticsModification}</b>\\n
+\\n
+Текущий температурный обмен: <b>{currentTransferWattage}</b>\\n
+\\n
+Толщина изоляции: {conductivityBarrier}`,
+		},
+	],
+},
+{
+	msgctxt: "STRINGS.DUPLICANTS.STATUSITEMS.HOT.TOOLTIP",
+	msgid: `This Duplicant cannot let off enough <style=\\"KKeyword\\">Heat</style> to stay cool and may be over-insulated for this area\\n\\nThey will begin to recover shortly after they leave this area\\n\\nStress Modification: <b>{StressModification}</b>\\nStamina: <b>{StaminaModification}</b>\\nAthletics: <b>{AthleticsModification}</b>\\n\\nCurrent Environmental Exchange: <b>{currentTransferWattage}</b>\\n\\nInsulation Thickness: {conductivityBarrier}`,
+	msgstr: `
+Этот дубликант не может достаточно охладиться, чтобы контролировать тепло и может быть изолирован от этой области\\n
+Изменение стресса: <b>{StressModification}</b>\\n
+\\n
+Выносливость: <b>{StaminaModification}</b>\\n
+\\n
+Атлетичность: <b>{AthleticsModification}</b>\\n
+\\n
+Текущий температурный обмен: <b>{currentTransferWattage}</b>\\n
+\\n
+Толщина изоляции: {conductivityBarrier}`,
+	klei: `
+Этот дубликант не может <style=\\"KKeyword\\">охладить</style> организм, и ему жарко. Возможно, толщина теплоизоляции слишком велика для этой зоны\\n
+\\n
+Изменение стрессовой реакции: <b>{StressModification}</b>\\n
+Выносливость: <b>{StaminaModification}</b>\\n
+Физическая форма: <b>{AthleticsModification}</b>\\n
+\\n
+Нынешняя скорость теплообмена с окружающей средой: <b>{currentTransferWattage}</b>\\n
+\\n
+Толщина теплоизоляции: {conductivityBarrier}`,
+	mem: [
+		{
+			msgid: `This Duplicant cannot let off enough <style=\\"KKeyword\\">Heat</style> to stay cool and may be over-insulated for this area\\n\\nStress Modification: <b>{StressModification}</b>\\nStamina: <b>{StaminaModification}</b>\\nAthletics: <b>{AthleticsModification}</b>\\n\\nCurrent Environmental Exchange: <b>{currentTransferWattage}</b>\\n\\nInsulation Thickness: {conductivityBarrier}`,
+			msgstr: `
+Этот дубликант не может достаточно охладиться, чтобы контролировать тепло и может быть изолирован от этой области\\n
+Изменение стресса: <b>{StressModification}</b>\\n
+\\n
+Выносливость: <b>{StaminaModification}</b>\\n
+\\n
+Атлетичность: <b>{AthleticsModification}</b>\\n
+\\n
+Текущий температурный обмен: <b>{currentTransferWattage}</b>\\n
+\\n
+Толщина изоляции: {conductivityBarrier}`,
+		},
+	],
+},
+{
+	msgctxt: "STRINGS.UI.FRONTEND.LOADSCREEN.NEWEST_SAVE",
+	msgid: `NEWEST`,
+	msgstr: `Последнее сохранение`,
+	klei: `Последнее сохранение`,
+	mem: [
+		{
+			msgid: `Newest Save`,
+			msgstr: `Последнее сохранение`,
+		},
+	],
+},
+{
+	msgctxt: "STRINGS.UI.OVERLAYS.HEATFLOW.COOLING_DUPE",
+	msgid: `Body Heat Loss {0}\\n\\nUncomfortably chilly surroundings`,
 	msgstr: ``,
 },
 {
-	msgctxt: "STRINGS.LORE.BUILDINGS.SET_OBJECT.TITLE",
-	msgid: `Reminder to Staff`,
+	msgctxt: "STRINGS.UI.OVERLAYS.HEATFLOW.HEATING_DUPE",
+	msgid: `Body Heat Loss {0}\\n\\nUncomfortably toasty surroundings`,
 	msgstr: ``,
 },
 {
-	msgctxt: "STRINGS.UI.OVERLAYS.RELATIVETEMPERATURE.BUTTON",
-	msgid: `Relative Temperature Overlay`,
+	msgctxt: "STRINGS.UI.OVERLAYS.HEATFLOW.NEUTRAL_DUPE",
+	msgid: `Comfort Zone {0}`,
 	msgstr: ``,
-	klei: `Относительная температурная схема`,
 },
 {
-	msgctxt: "STRINGS.UI.OVERLAYS.RELATIVETEMPERATURE.HOVERTITLE",
-	msgid: `RELATIVE TEMPERATURE`,
-	msgstr: ``,
-	klei: `ОТНОСИТЕЛЬНАЯ ТЕМПЕРАТУРА`,
+	msgctxt: "STRINGS.UI.OVERLAYS.HEATFLOW.TOOLTIPS.COOLING",
+	msgid: `<b>Body Heat Loss</b>\\nUncomfortably cold\\n\\nDuplicants lose more heat in chilly surroundings than they can absorb\\n    • Warm Coats help Duplicants retain body heat`,
+	msgstr: `
+<b>Потери тепла</b>\\n
+Некомфортно и холодно\\n
+\\n
+Дубликанты теряют больше тепла, чем вырабатывают\\n
+* Тёплые свитера помогут им сохранть больше тепла`,
+	klei: `
+<b>Потеря тепла</b>\\n
+Слишком холодно\\n
+\\n
+В этих областях дубликанты теряют больше тепла, чем получают\\n
+*Теплое пальто позволит сохранить тепло тела`,
+	mem: [
+		{
+			msgid: `
+<b>Body Heat Loss</b>\\n
+Uncomfortably cold\\n
+------------------\\n
+Duplicants lose more heat in these areas than they can absorb\\n
+* Warm Sweaters help Duplicants retain body heat`,
+			msgstr: `
+<b>Потеря тепла тела</b>\\n
+Некомфортный холод\\n
+------------------\\n
+Дубликанты теряют больше тепла в этих областях, чем поглощают\\n
+* Теплые свитера помогут дубликантам сохранить тепло тела`,
+		},
+		{
+			msgid: `<b>Body Heat Loss</b>\\nUncomfortably cold\\n\\nDuplicants lose more heat in these areas than they can absorb\\n* Warm Coats help Duplicants retain body heat`,
+			msgstr: `
+<b>Потери тепла</b>\\n
+Некомфортно и холодно\\n
+\\n
+Дубликанты теряют больше тепла, чем вырабатывают\\n
+* Тёплые свитера помогут им сохранть больше тепла`,
+		},
+	],
 },
 {
-	msgctxt: "STRINGS.UI.OVERLAYS.RELATIVETEMPERATURE.NAME",
-	msgid: `RELATIVE TEMPERATURE`,
-	msgstr: ``,
-	klei: `ОТНОСИТЕЛЬНАЯ ТЕМПЕРАТУРА`,
+	msgctxt: "STRINGS.UI.OVERLAYS.HEATFLOW.TOOLTIPS.HEATING",
+	msgid: `<b>Body Heat Retention</b>\\nUncomfortably warm\\n\\nDuplicants absorb more heat in toasty surroundings than they can release`,
+	msgstr: `
+<b>Перегрев</b>\\n
+Некомфортно и жарко\\n
+\\n
+Дубликанты поглощают больше тепла, чем могут выделить\\n
+* Тёплые свитера помогут дубликантам поглощать меньше тепла`,
+	klei: `
+<b>Сохранение тепла</b>\\n
+Слишком тепло\\n
+\\n
+В этих областях дубликанты получают больше тепла, чем отдают`,
+	mem: [
+		{
+			msgid: `
+<b>Body Heat Retention</b>\\n
+Uncomfortably warm\\n
+------------------\\n
+Duplicants absorb more heat in these areas than they can release\\n
+* Cool Vests help Duplicants shed excess body heat`,
+			msgstr: `
+<b>Сохранение тепла тела</b>\\n
+Некомфортная жара\\n
+------------------\\n
+Дубликанты поглощают больше тепла в этих областях, чем выделяют\\n
+* Холодные жилеты помогают дубликантам терять избыток тепла тела`,
+		},
+		{
+			msgid: `<b>Body Heat Retention</b>\\nUncomfortably warm\\n\\nDuplicants absorb more heat in these areas than they can release`,
+			msgstr: `
+<b>Перегрев</b>\\n
+Некомфортно и жарко\\n
+\\n
+Дубликанты поглощают больше тепла, чем могут выделить\\n
+* Тёплые свитера помогут дубликантам поглощать меньше тепла`,
+		},
+	],
 },
 {
-	msgctxt: "STRINGS.UI.TOOLS.FILTERLAYERS.ADAPTIVETEMPERATURE.NAME",
-	msgid: `Adapt. Temperature`,
+	msgctxt: "STRINGS.UI.SPACEDESTINATIONS.COMETS.MINICOMET.NAME",
+	msgid: `Debris Meteor`,
 	msgstr: ``,
-	klei: `Адапт. температура`,
 },
 ];
